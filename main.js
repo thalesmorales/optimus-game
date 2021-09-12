88ae0cd756d199caad5fc6939f76641a91811f50
Victor Matheus <victor.matheus@protonmail.com>
2021-09-12 15:15:48 -0300

    Movimentação do astronauta na fase 1

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
 window.onload = function () {
-  let game = new Phaser.Game(800, 600, Phaser.CANVAS, "", {
+  let game = new Phaser.Game(1885, 400, Phaser.AUTO, "", {
     preload: preload,
     create: create,
+    update: update,
   });
 
   function preload() {
-    game.load.image("logo", "assets/astronauta.png");
+    game.load.image("planet", "assets/fase01_background-01.png");
+
+    game.load.spritesheet(
+      "astronaut",
+      "assets/astronauta145x298.png",
+      145,
+      298,
+      7
+    );
   }
 
+  let astronaut;
+  let back;
+
   function create() {
-    let s = game.add.sprite(80, 0, "logo");
-    s.rotation = 0.14;
+    back = game.add.image(0, 0, "planet");
+    back.scale.set(1);
+    back.smoothed = false;
+
+    astronaut = game.add.sprite(-24, 128, "astronaut");
+    astronaut.scale.set(1);
+    astronaut.smoothed = false;
+    astronaut.animations.add("run");
+    astronaut.animations.play("run", 10, true);
+  }
+
+  function update() {
+    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) astronaut.x -= 4;
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+      astronaut.x += 4;
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) astronaut.y -= 4;
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) astronaut.y += 4;
+  }
+
+  function render() {
+    game.debug.sprite(astronaut, 20, 32);
   }
 };
